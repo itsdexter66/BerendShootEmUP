d00f7234a46937785eab332255ff831d605797b9
DESKTOP-0BTH82I\Gebruiker <mjakobs11@hotmail.nl>
2019-01-11 10:25:32 +0100

    Score
    Click for score

--- BerendShootEmUp/Assets/Scripts/KillEnemy.cs
+++ BerendShootEmUp/Assets/Scripts/KillEnemy.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KillEnemy : MonoBehaviour {
-
+    public int scoreValue = 1;
     void OnMouseDown()
     {
         Destroy(gameObject);
+
+        // Increase the score by the enemy's score value.
+        ScoreManager.score += scoreValue;
+
     }
 }
